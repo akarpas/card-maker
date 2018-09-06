628c11e8dfd0371d6faeab704f09a5c44dd24794
Alberto Gallego <albertogc2793@gmail.com>
2018-09-06 16:56:35 +0200

    Added redux

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Provider } from "react-redux";
 
-class App extends Component {
+import reducers from "./app/reducers";
+
+export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Provider store={reducers}>
+        <h1>Welcome to Colvin React Coding Challenge</h1>
+      </Provider>
     );
   }
 }
-
-export default App;
