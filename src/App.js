1daf16593140a9cf9b40cfd54473b0b451567694
Alberto Gallego <albertogc2793@gmail.com>
2018-09-06 16:59:53 +0200

    Removed unnecesary assets

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
 import { Provider } from "react-redux";
-
-import reducers from "./app/reducers";
+import store from "./store";
 
 export default class App extends Component {
   render() {
     return (
-      <Provider store={reducers}>
+      <Provider store={store}>
         <h1>Welcome to Colvin React Coding Challenge</h1>
       </Provider>
     );
---
+++ src/store.js
@@ -0,0 +1,5 @@
+import { combineReducers } from "redux";
+
+const store = combineReducers({});
+
+export default store;
