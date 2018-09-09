225841a5174734fb8d57f943d6517f72feaf770c
Andreas Karpasitis <karpasitis@outlook.com>
2018-09-09 22:37:08 +0200

    make form collapsile - move styled components to separate file

---
+++ src/components/CardMaker/index.js
@@ -0,0 +1,95 @@
+import React, { Component } from "react";
+import { connect } from "react-redux"
+import { 
+  Container,
+  DrawerButton,
+  TextArea,
+  Label,
+  StyledInput,
+  StyledSelect,
+  Form,
+  Button
+} from './style'
+import ExpandLessIcon from "../../assets/expandLess.png"
+import ExpandMoreIcon from "../../assets/expandMore.png"
+import * as actions from '../../actions'
+
+class CardMaker extends Component {
+  state = {
+    text: '',
+    author: '',
+    emoji: 'smiley',
+    open: true
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.props.saveCard(this.state)
+    this.setState({
+      text: '',
+      author: '',
+      emoji: 'smiley'
+    })
+  }
+
+  handleChange = (event) => {
+    const { name, value } = event.target
+    this.setState({ [name]: value })
+  }
+
+  handleClick = (event) => {
+    const { open } = this.state
+    this.setState({ open: !open })
+  }
+
+  render() {
+    const { open } = this.state
+
+    return (
+      <Container>
+        {!open && <DrawerButton src={ExpandMoreIcon} onClick={this.handleClick}/>}
+        {open && 
+          <Form id="form" onSubmit={this.handleSubmit}>
+            <Label>Card Text</Label>
+            <TextArea
+              id="text"
+              name="text"
+              type="textarea"
+              placeholder="Add text to your card"
+              value={this.state.text}
+              onChange={this.handleChange}
+              required
+            />
+            <Label>Author</Label>
+            <StyledInput
+              name="author"
+              component="input"
+              type="text"
+              value={this.state.author}
+              onChange={this.handleChange}
+              placeholder="Name"
+              required
+            />
+            <Label>Mood</Label>
+            <StyledSelect
+              name="emoji"
+              required
+              component="select"
+              onChange={this.handleChange}
+              value={this.state.emoji}
+            >
+              <option value="smiley">Smiley</option>
+              <option value="angry">Angry</option>
+              <option value="sad">Sad</option>
+            </StyledSelect>
+            <Button type="submit"> Create </Button>
+            <DrawerButton src={ExpandLessIcon} onClick={this.handleClick}/>
+          </Form>
+        }
+      </Container>
+    );
+  }
+}
+
+
+export default connect(null, actions)(CardMaker)
---
+++ src/components/CardMaker/style.js
@@ -0,0 +1,50 @@
+import styled from "styled-components"
+
+ export const Label = styled.label`
+  font-size: 20px;
+  padding: 10px 0 10px 0;
+`
+
+ export const TextArea = styled.textarea`
+  font-size: 20px;
+`
+
+ export const StyledInput = styled.input`
+  font-size: 20px;
+`
+
+ export const StyledSelect = styled.select`
+  font-size: 20px;
+  background-color: white;
+  border-radius: 0;
+`
+
+ export const Button = styled.button`
+  border: 2px solid white;
+  background-color: grey;
+  padding: 10px;
+  margin: 20px;
+  font-size: 30px;
+`
+
+ export const DrawerButton = styled.img`
+  cursor: pointer;
+`
+
+ export const Container = styled.div`
+  border: 1px solid white;
+  border-radius: 2%;
+  padding: 1vh 1vw;
+  width: 90%;
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  align-items: center;
+`
+
+ export const Form = styled.form`
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+  padding: 20px;
+`
