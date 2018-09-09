2d68da6b64a9b900bbe9b1ec170934f13b102e30
Andreas Karpasitis <karpasitis@outlook.com>
2018-09-09 15:34:31 +0200

    create basic form for CardMaker

--- src/components/CardMaker.js
+++ src/components/CardMaker.js
@@ -1,11 +1,103 @@
 import React, { Component } from "react";
+import styled from 'styled-components'
 
-export default class CardMaker extends Component {
+const Container = styled.div`
+  border: 1px solid white;
+  border-radius: 2%;
+  padding: 1vh 1vw;
+  width: 90%;
+`
+const Form = styled.form`
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+  padding: 20px;
+`
+
+const Label = styled.label`
+  font-size: 20px;
+  padding: 10px 0 10px 0;
+`
+
+const TextArea = styled.textarea`
+  font-size: 30px;
+`
+
+const StyledInput = styled.input`
+  font-size: 30px;
+`
+
+const StyledSelect = styled.select`
+  font-size: 30px;
+  background-color: white;
+  width: 100%;
+`
+
+const Button = styled.button`
+  border: 2px solid white;
+  background-color: grey;
+  padding: 10px;
+  margin: 20px;
+  font-size: 30px;
+`
+class CardMaker extends Component {
+  state = {
+    text: '',
+    author: '',
+    emoji: 'smiley'
+  }
+
+  handleSubmit = (event) => {
+    this.setState({
+      text: '',
+      author: '',
+      emoji: 'smiley'
+    })
+    event.preventDefault()
+  }
+
+  handleChange = (event) => {
+    const { name, value } = event.target
+    this.setState({ [name]: value })
+  }
+
   render() {
     return (
-      <div>
-        <h1>Card Maker</h1>
-      </div>
+      <Container>
+        <Form onSubmit={this.handleSubmit}>
+          <Label>Card Text</Label>
+          <TextArea
+            name="text"
+            type="textarea"
+            placeholder="Add text to your card"
+            value={this.state.text}
+            onChange={this.handleChange}
+          />
+          <Label>Author</Label>
+          <StyledInput
+            name="author"
+            component="input"
+            type="text"
+            value={this.state.author}
+            onChange={this.handleChange}
+            placeholder="Name"
+          />
+          <Label>Mood</Label>
+          <StyledSelect
+            name="emoji"
+            component="select"
+            onChange={this.handleChange}
+            value={this.state.emoji}
+          >
+            <option value="smiley">Smiley</option>
+            <option value="angry">Angry</option>
+            <option value="sad">Sad</option>
+          </StyledSelect>
+          <Button type="submit"> Create </Button>
+        </Form>
+      </Container>
     );
   }
 }
+
+export default (CardMaker)
