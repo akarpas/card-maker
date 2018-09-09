89763219cf3ef2f9c76af5243aa8cb629e55283d
Andreas Karpasitis <karpasitis@outlook.com>
2018-09-09 14:15:50 +0200

    add styled components to main APP

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,27 @@ import Title from './Title'
 import CardEditor from './CardEditor'
 import CardTools from './CardTools'
 import CardList from './CardList'
+import styled from 'styled-components'
 
+const Wrapper = styled.div`
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  padding: 1vh 1vw;
+  color: white;
+  background-color: black;
+`
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <Wrapper>
         <Title />
         <CardEditor />
         <CardTools />
         <CardList />
-      </div>
+      </Wrapper>
     );
   }
 }
