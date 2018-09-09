6aba643561f3c53ed7f545036747803ee62308af
Andreas Karpasitis <karpasitis@outlook.com>
2018-09-09 13:58:59 +0200

    add basic test to test components

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { shallow } from 'enzyme'
 import ReactDOM from 'react-dom';
 import App from '../App';
+import Title from '../Title'
+import CardEditor from '../CardEditor'
+import CardTools from '../CardTools'
+import CardList from '../CardList'
+
+let wrapped
+
+beforeEach(() => {
+  wrapped = shallow(<App />)
+})
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  wrapped.render()
 });
+
+it('includes 4 components', () => {
+  expect(wrapped.find(Title).length).toEqual(1)
+  expect(wrapped.find(CardEditor).length).toEqual(1)
+  expect(wrapped.find(CardList).length).toEqual(1)
+  expect(wrapped.find(CardTools).length).toEqual(1)
+})
