037d4c2747f9d538ec57146614b98715a10c5a22
Andreas Karpasitis <karpasitis@outlook.com>
2018-09-09 18:50:11 +0200

    setup wireframe for CardList component to list cards

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,11 +1,25 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
-export default class CardList extends Component {
+class CardList extends Component {
   render() {
     return (
       <div>
         <h1>Card List</h1>
+        <ul>
+          {
+            this.props.cards.map(item => {
+              return <li>{item.emoji}</li>
+            })
+          }
+        </ul>
       </div>
     );
   }
 }
+
+const mapStateToProps = (state) => {
+  return { cards: state.cards }
+}
+
+export default connect(mapStateToProps)(CardList)
