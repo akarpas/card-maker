bc1ab647c52a83cc0c87cf4ac25cc79403bbea1d
Andreas Karpasitis <karpasitis@outlook.com>
2018-09-09 19:36:50 +0200

    upadte list styling

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,21 +1,53 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import styled from "styled-components"
+
+const Container = styled.div`
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+  border: 1px solid white;
+  border-radius: 2%;
+  padding: 1vh 1vw;
+  margin: 2vh 0;
+  width: 90%;
+`
+
+const Cards = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+`
+
+const Card = styled.div`
+  height: 250px;
+  min-width: 35%;
+  border: 1px solid white;
+  @media screen and (min-width:1000px) {
+    min-width: 22%;
+  }
+  @media screen and (min-width:1600px) {
+    min-width: 10%;
+  }
+`
 
 class CardList extends Component {
   render() {
     const { cards } = this.props
 
     return (
-      <div>
+      <Container>
         <h1>Card List</h1>
-        <ul>
+        <Cards>
           {
             cards.map((item, index) => {
-              return <li key={`${item.text}-${index}`}>{item.emoji}</li>
+              return <Card key={`${item.text}-${index}`}>{item.emoji}</Card>
             })
           }
-        </ul>
-      </div>
+        </Cards>
+      </Container>
     );
   }
 }
