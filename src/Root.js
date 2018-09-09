bd29f93036a1d06bb370ca5b3896875af7bcb33c
Andreas Karpasitis <karpasitis@outlook.com>
2018-09-09 19:36:24 +0200

    set initial state with json file

--- src/Root.js
+++ src/Root.js
@@ -2,8 +2,11 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import reducers from './reducers'
+import CardsJSON from '../src/assets/cards.json'
 
-export default ({ children, initialState = {} }) => {
+const { cards } = CardsJSON
+
+export default ({ children, initialState = { cards } }) => {
   return (
     <Provider store={createStore(reducers, initialState)}>
       {children}
