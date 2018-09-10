cf5bc0a010c96c5207b864cbbff90fb7e61bbdd3
Andreas Karpasitis <karpasitis@outlook.com>
2018-09-10 12:26:30 +0200

    fix delete action

--- src/reducers/cards.js
+++ src/reducers/cards.js
@@ -1,10 +1,12 @@
 import { SAVE_CARD, DELETE_CARD, EDIT_CARD } from "../actions/types"
 
 const deleteCard = (state, data) => {
+  console.warn("STATE ", state[0])
   if (state.length === 1) {
     return ([])
   }
-  return (state.splice(data.cardNumber, 1))
+  state.splice(data.cardNumber, 1)
+  return [...state]
 }
 
 const editCard = (state, data) => {
