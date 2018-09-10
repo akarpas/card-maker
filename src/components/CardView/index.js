2fa48b8784448e01c73770df8c7750fb9908c6f4
Andreas Karpasitis <karpasitis@outlook.com>
2018-09-10 11:23:39 +0200

    setup cardview functionality

--- src/components/CardView/index.js
+++ src/components/CardView/index.js
@@ -1,12 +1,49 @@
 import React, { Component } from "react";
-import { ModalContainer, ModalContent, Card } from "./style.js"
+import {
+  ModalContainer,
+  ModalContent,
+  Card,
+  Controls,
+  Control,
+  CardText,
+  AuthorText,
+  Emoji
+} from "./style.js"
 
 export default class CardView extends Component {
+  state = {
+    text: '',
+    author: '',
+    emoji: ''
+  }
+
+  componentDidMount() {
+    const { state, pathname } = this.props.location
+    const cardNumber = pathname.split('/')[2]
+
+    this.setState({ ...state[cardNumber] })
+  }
+
+  handleChange = (event) => {
+    event.preventDefault()
+    this.setState({ text: event.target.value })
+  }
+
   render() {
+    const { text, author, emoji } = this.state
     return (
       <ModalContainer>
         <ModalContent>
-          <Card>Card</Card>
+          <Card>
+            <CardText value={text} onChange={this.handleChange}/>
+            <AuthorText>Author: {author}</AuthorText>
+            <Emoji>Mood: {emoji}</Emoji>
+          </Card>
+          <Controls>
+            <Control>Delete</Control>
+            <Control>Save</Control>
+            <Control>Close</Control>
+          </Controls>
         </ModalContent>
       </ModalContainer>
     );
