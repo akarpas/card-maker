6bea1085680a329f15589604778c443fe509ceeb
Andreas Karpasitis <karpasitis@outlook.com>
2018-09-10 09:49:39 +0200

    make CardMaker test more specific - use mount

--- src/components/__tests__/CardMaker.test.js
+++ src/components/__tests__/CardMaker.test.js
@@ -1,41 +1,44 @@
 import React from 'react';
-import { shallow } from 'enzyme'
-import CardMaker from '../CardMaker/index'
+import Root from '../../Root'
+import { mount } from 'enzyme'
+import CardMaker from '../CardMaker/index.js'
 
 let wrapped
 
 beforeEach(() => {
-  wrapped = shallow(<CardMaker />)
+  wrapped = mount(
+    <Root>
+      <CardMaker />
+    </Root>
+  )
 })
 
 it('renders without crashing', () => {
   wrapped.render()
 });
 
-it('includes a form with three fields', () => {
-  expect(wrapped.find('[name="text"]').length).toEqual(1)
-  expect(wrapped.find('[type="textarea"]').length).toEqual(1)
-  expect(wrapped.find('[name="author"]').length).toEqual(1)
-  expect(wrapped.find('[type="text"]').length).toEqual(1)
-  expect(wrapped.find('[name="emoji"]').length).toEqual(1)
+it('includes a form with three inputs and three options for select', () => {
+  expect(wrapped.find('textarea').length).toEqual(1)
+  expect(wrapped.find('input').length).toEqual(1)
+  expect(wrapped.find('select').length).toEqual(1)
   expect(wrapped.find('option').length).toEqual(3)
 })
 
 
 describe('the text area', () => {
   it('includes a submit button', () => {
-    expect(wrapped.find('[type="submit"]').length).toEqual(1)
+    expect(wrapped.find('button').length).toEqual(1)
   })
 
   it('resets inputs on submit', () => {
-    wrapped.find('[type="text"]').simulate('change', {
+    wrapped.find('textarea').simulate('change', {
       target: { value: 'Some text' }
     })
     wrapped.update()
-    wrapped.find('[type="submit"]').simulate('submit')
+    wrapped.find('button').simulate('submit')
     wrapped.update()
-    expect(wrapped.find('[name="text"]').prop('value')).toEqual('')
-    expect(wrapped.find('[name="author"]').prop('value')).toEqual('')
-    expect(wrapped.find('[name="emoji"]').prop('value')).toEqual('smiley')
+    expect(wrapped.find('textarea').prop('value')).toEqual('')
+    expect(wrapped.find('input').prop('value')).toEqual('')
+    expect(wrapped.find('select').prop('value')).toEqual('smiley')
   })
 })
