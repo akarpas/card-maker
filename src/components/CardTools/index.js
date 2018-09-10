3a683a33c1de8586ecce4ab9a8554f8310c4859f
Andreas Karpasitis <karpasitis@outlook.com>
2018-09-10 16:10:20 +0200

    setup CardTools basic component with functional reset

--- src/components/CardTools/index.js
+++ src/components/CardTools/index.js
@@ -1,11 +1,25 @@
 import React, { Component } from "react";
+import { connect } from "react-redux"
+import { Container, Button } from "./style"
+import SaveIcon from "../../assets/save.png"
+import ExportIcon from "../../assets/export.png"
+import ReloadIcon from "../../assets/reload.png"
+import * as actions from "../../actions"
 
-export default class CardTools extends Component {
+class CardTools extends Component {
+  clear = event => {
+    event.preventDefault()
+    this.props.eraseCards(this.state)
+  }
   render() {
     return (
-      <div>
-        <h1>Card Tools</h1>
-      </div>
+      <Container>
+        <Button onClick={this.clear} src={ReloadIcon}></Button>
+        <Button src={SaveIcon}></Button>
+        <Button src={ExportIcon}></Button>
+      </Container>
     );
   }
 }
+
+export default connect(null, actions)(CardTools)
---
+++ src/components/CardTools/style.js
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+
+ export const Container = styled.label`
+  display: flex;
+  flex-flow: row;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+  width: 30%;
+  margin: 2vh 0;
+`
+
+export const Button = styled.img`
+  cursor: pointer;
+  height: 35px;
+  margin: 0 2vw;
+`
