f29c4f9049a0a63f724e13ea2b1b618fb76e4875
Andreas Karpasitis <karpasitis@outlook.com>
2018-09-11 12:39:33 +0200

    implement basic redux form and import to CardMaker

--- src/components/CardMaker/index.js
+++ src/components/CardMaker/index.js
@@ -1,16 +1,10 @@
 import React, { Component } from "react";
 import { connect } from "react-redux"
+import ReduxForm from "../ReduxForm"
 import { 
   Container,
-  DrawerButton,
-  TextArea,
-  Label,
-  StyledInput,
-  StyledSelect,
-  Form,
-  Button
+  DrawerButton
 } from "./style"
-import ExpandLessIcon from "../../assets/expandLess.png"
 import ExpandMoreIcon from "../../assets/expandMore.png"
 import * as actions from "../../actions"
 
@@ -18,80 +12,57 @@ class CardMaker extends Component {
   state = {
     text: "",
     author: "",
-    emoji: "smiley",
+    emoji: "",
     open: true
   }
 
-  handleSubmit = (event) => {
-    event.preventDefault()
-    const { text, author, emoji } = this.state
+  handleSubmit = values => {
+    if (!values.emoji) {
+      values.emoji = "smiley"
+    }
+    const { text, author, emoji } = values
     this.props.saveCard({
       text,
       author,
       emoji
     })
+    this.props.clearForm()
+    // this.props.clearForm()
     this.setState({
       text: "",
       author: "",
-      emoji: "smiley"
+      emoji: ""
     })
   }
 
-  handleChange = (event) => {
+  handleChange = event => {
     const { name, value } = event.target
     this.setState({ [name]: value })
   }
 
-  handleClick = (event) => {
+  handleClick = () => {
     const { open } = this.state
     this.setState({ open: !open })
   }
 
   render() {
-    const { open } = this.state
+    const { open, text, author, emoji } = this.state
 
     return (
       <Container>
         {!open && <DrawerButton src={ExpandMoreIcon} onClick={this.handleClick}/>}
         {open && 
-          <Form id="form" onSubmit={this.handleSubmit}>
-            <Label>Card Text</Label>
-            <TextArea
-              name="text"
-              placeholder="Add text to your card"
-              value={this.state.text}
-              onChange={this.handleChange}
-              required
-            />
-            <Label>Author</Label>
-            <StyledInput
-              name="author"
-              component="input"
-              value={this.state.author}
-              onChange={this.handleChange}
-              placeholder="Name"
-              required
-            />
-            <Label>Mood</Label>
-            <StyledSelect
-              name="emoji"
-              required
-              component="select"
-              onChange={this.handleChange}
-              value={this.state.emoji}
-            >
-              <option value="smiley">Smiley</option>
-              <option value="angry">Angry</option>
-              <option value="sad">Sad</option>
-            </StyledSelect>
-            <Button type="submit"> Create </Button>
-            <DrawerButton src={ExpandLessIcon} onClick={this.handleClick}/>
-          </Form>
+          <ReduxForm
+            onSubmit={this.handleSubmit}
+            handleClick={this.handleClick}
+            text={text}
+            author={author}
+            emoji={emoji}
+          />
         }
       </Container>
     );
   }
 }
-
 
 export default connect(null, actions)(CardMaker)
---
+++ src/components/ReduxForm/style.js
@@ -0,0 +1,61 @@
+import styled from "styled-components"
+import { Field } from "redux-form"
+
+export const Label = styled.label`
+  font-size: 20px;
+  padding: 10px 0 10px 0;
+`
+
+export const TextArea = styled(Field)`
+  resize: none;
+  font-size: 20px;
+  height: 100px;
+  min-height: 80px;
+  width: 80vw;
+  border-radius: 10px;
+  font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+  @media screen and (min-width:1200px) {
+    width: 30vw;
+  }
+`
+
+export const StyledInput = styled(Field)`
+  font-size: 20px;
+  border-radius: 10px;
+`
+
+export const StyledSelect = styled(Field)`
+  font-size: 20px;
+  background-color: white;
+  border-radius: 0;
+`
+
+export const Button = styled.button`
+  cursor: pointer;
+  border: 2px solid white;
+  background-color: grey;
+  padding: 10px;
+  margin: 20px;
+  font-size: 30px;
+  border-radius: 10px;
+  &:hover {
+    background-color: lightgrey;
+  }
+  &:active {
+    background-color: grey;
+  }
+`
+
+export const DrawerButton = styled.img`
+  cursor: pointer;
+  @media screen and (min-width:1200px) {
+    display: none
+  }
+`
+
+export const Form = styled.form`
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+  padding: 20px;
+`
