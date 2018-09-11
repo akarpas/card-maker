9c310cf71d52445476b85fdcba9867ed6f211c74
Andreas Karpasitis <karpasitis@outlook.com>
2018-09-11 11:16:48 +0200

    add test for EDIT_CARD

--- src/reducers/__tests__/cards.test.js
+++ src/reducers/__tests__/cards.test.js
@@ -1,5 +1,5 @@
 import cardsReducer from '../cards'
-import { SAVE_CARD, DELETE_CARD } from '../../actions/types'
+import { SAVE_CARD, DELETE_CARD, EDIT_CARD } from '../../actions/types'
 
 it('handles actions of type SAVE_CARD', () => {
   const card = {
@@ -35,7 +35,33 @@ it('handles actions of type DELETE_CARD', () => {
   }
   const newState = cardsReducer(initialCards, action)
   expect(newState).toEqual([initialCards[0]])
-}) 
+})
+
+it('handles actions of type EDIT_CARD', () => {
+  const cardNumber = 1
+  const payload = {
+    cardNumber,
+    text: 'card has been edited'
+  }
+  const initialCards = [
+    {
+      text: 'keep this card',
+      author: 'important',
+      emoji: 'smiley'
+    },
+    {
+      text: 'edit this card',
+      author: 'edit',
+      emoji: 'smiley'
+    }
+  ]
+  const action = {
+    type: EDIT_CARD,
+    payload
+  }
+  const newState = cardsReducer(initialCards, action)
+  expect(newState[cardNumber].text).toEqual(payload.text)
+})
 
 it('handles action with unknown type', () => {
   const newState = cardsReducer([], { type: 'unknown' })
