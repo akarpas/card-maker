4bd2f3873e0acfb9c5e87f41dcc5304b38ff8cfe
Andreas Karpasitis <karpasitis@outlook.com>
2018-09-11 16:21:31 +0200

    add to pdf functionality

--- src/components/PrintView/index.js
+++ src/components/PrintView/index.js
@@ -2,10 +2,22 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Container, Cards, Line, Card, Button, Overlay } from "./style"
 import SaveIcon from "../../assets/save.png"
+import html2canvas from "html2canvas"
+import jsPDF from "jspdf"
 
 class PrintView extends Component {
   print = () => {
-
+    const input = document.getElementById('divToPrint');
+    console.warn(input.attributes)
+    html2canvas(input)
+      .then((canvas) => {
+        console.warn(canvas)
+        const imgData = canvas.toDataURL('image/png');
+        const pdf = new jsPDF();
+        pdf.addImage(imgData, 'JPEG', 0, 0);
+        pdf.save("download.pdf");
+      })
+    ;
   }
 
   render() {
