ed31e0eff5af70bf8abeeb1cb54afab626ad2322
Andreas Karpasitis <andreas.karpasitis@utopiamusic.com>
2018-09-13 16:24:45 +0200

    refactor reducer index

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,17 +1,5 @@
 import { combineReducers } from "redux"
 import cardsReducer from "./cards"
-import { reducer as formReducer } from "redux-form"
-import { CARD_CREATED } from "../actions/types.js"
+import formsReducer from "./forms"
 
-const form = formReducer.plugin({
-  create: (state, action) => {
-    switch(action.type) {
-      case CARD_CREATED:
-        return undefined
-      default:
-        return state
-    }
-  }
-})
-
-export default combineReducers({ cards: cardsReducer, form })
+export default combineReducers({ cards: cardsReducer, form: formsReducer })
